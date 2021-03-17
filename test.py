86956de73f174d543e96131dd22503db3210c964
jaydattd <78740290+jaydattd@users.noreply.github.com>
2021-03-17 19:35:41 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from concurrent import futures
+import grpc
+
+import Comp_a_pb2
+import Comp_a_pb2_grpc
+
+channel = grpc.insecure_channel('localhost:50055')
+stub = Comp_a_pb2_grpc.CompAStub(channel)
+
+componentAResponse = stub.getData(Comp_a_pb2.GetFullnameRequest(firstName="test1"))
+
+print(componentAResponse)
