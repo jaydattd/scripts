7bc3d54a2f21cd20497a4e0e6a251ab34a977924
jaydattd <78740290+jaydattd@users.noreply.github.com>
2021-03-17 22:22:36 +0530

    Update setup-mock-serveer.sh

--- setup-mock-serveer.sh
+++ setup-mock-serveer.sh
@@ -1,1 +1,15 @@
 docker run -p 4770:4770 -p 4771:4771 -v $PWD/src/resources/protos/:/proto tkpd/gripmock /proto/Comp-b.proto
+curl -X POST -d '{
+    "service":"CompB",
+    "method":"getData",
+    "input":{
+      "equals":{
+        "firstName":"Jaydatt"
+      }
+    },
+    "output":{
+      "data":{
+        "lastName":"Desai"
+      }
+    }
+  }'
