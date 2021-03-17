e86ba795f93dd2e26b732676960a99efad5327aa
jaydattd <78740290+jaydattd@users.noreply.github.com>
2021-03-17 19:34:39 +0530

    Update test-grpc-code-generator.sh

--- test-grpc-code-generator.sh
+++ test-grpc-code-generator.sh
@@ -1,3 +1,2 @@
-cd test
 python3 -m grpc_tools.protoc -I./resources/protos --python_out=. --grpc_python_out=. ./resources/protos/*
-cp test.py test/*
+wget https://raw.githubusercontent.com/jaydattd/scripts/main/test.py
