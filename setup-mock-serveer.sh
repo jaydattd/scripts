e2c178437b4e6afcd79017b06ad936d524609218
jaydattd <78740290+jaydattd@users.noreply.github.com>
2021-03-18 18:41:34 +0530

    Update setup-mock-serveer.sh

--- setup-mock-serveer.sh
+++ setup-mock-serveer.sh
@@ -1,9 +1,8 @@
-export PWD=pwd
-CONTAINER_ID=`docker run -p 4770:4770 -p 4771:4771 -v $PWD/../src/resources/protos/:/proto tkpd/gripmock $1`
+CONTAINER_ID=`docker run --name grpc-mock-server -p 4770:4770 -p 4771:4771 -v $(pwd)/../src/resources/protos/:/proto tkpd/gripmock $1`
 
-until [ "`/usr/bin/docker inspect -f {{.State.Status}} mock-server`"=="running" ]; do
-    sleep 0.1;
-done;
+# until [ "`docker inspect -f {{.State.Running}} grpc-mock-server`"=="true" ]; do
+#     sleep 0.1;
+# done;
 
-value=`cat $2`
-curl -X POST -d '$value'  http://localhost:4771/add
+# value=`cat $2`
+# curl -X POST -d '$value'  http://localhost:4771/add
