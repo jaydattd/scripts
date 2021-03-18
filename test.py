a4b2e59e8817300b1248e0a2282b9be906e3ebfc
jaydattd <78740290+jaydattd@users.noreply.github.com>
2021-03-18 18:59:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ import Comp_a_pb2_grpc
 channel = grpc.insecure_channel('localhost:50055')
 stub = Comp_a_pb2_grpc.CompAStub(channel)
 
-componentAResponse = stub.getData(Comp_a_pb2.GetFullnameRequest(firstName="test1"))
+componentAResponse = stub.getData(Comp_a_pb2.GetFullnameRequest(firstName="Jaydatt"))
 
 print(componentAResponse)
